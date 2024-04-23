90671d77e45192cc246bb7e9d8be83e1bcb59639
mardbrito <marcelo4web@gmail.com>
2024-04-23 19:54:27 -0400

    Computing income and expenses on component passing by props

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,29 @@ const total = computed(() => {
   }, 0);
 })
 
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0).toFixed(2);
+});
+
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0).toFixed(2);
+});
+
 </script>
 
 <template>
   <Header />
   <div class="container">
     <Balance :total="total" />
-    <IncomeExpenses />
+    <IncomeExpenses :income="income" :expenses="expenses" />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -1,12 +1,25 @@
+<script setup>
+const props = defineProps({
+    income: {
+        type: Number,
+        required: true
+    },
+    expenses: {
+        type: Number,
+        required: true
+    }
+})
+</script>
+
 <template>
     <div class="inc-exp-container">
         <div>
             <h4>Income</h4>
-            <p id="money-plus" class="money plue">+$0.00</p>
+            <p id="money-plus" class="money plue">+$ {{ income }}</p>
         </div>
         <div>
             <h4>Expenses</h4>
-            <p id="money-minus" class="money minus">-$0.00</p>
+            <p id="money-minus" class="money minus">-$ {{ expenses }}</p>
         </div>
     </div>
 </template>
