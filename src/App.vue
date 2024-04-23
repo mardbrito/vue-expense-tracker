4b879beba922cdb891c3a35bf9dfd77c62f27783
mardbrito <marcelo4web@gmail.com>
2024-04-23 18:02:03 -0400

    Base of the UI

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AddTransaction from "./components/AddTransaction.vue";
+import Balance from "./components/Balance.vue";
+import Header from "./components/Header.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+</script>
+
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
