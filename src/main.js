32ef01e23b033c2ec8b9a81f50b3720cb537b01f
mardbrito <marcelo4web@gmail.com>
2024-04-24 17:37:36 -0400

    Configure toast to work in all application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/style.css'   
+import './assets/style.css';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).use(Toast).mount('#app');
