4b37bd033101a5ba6ba44c5c04cb84a298f82de6
mardbrito <marcelo4web@gmail.com>
2024-04-24 17:44:20 -0400

    Add transaction to state

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { computed, ref } from 'vue';
+import { useToast } from 'vue-toastification'
 
 import AddTransaction from "./components/AddTransaction.vue";
 import Balance from "./components/Balance.vue";
 import Header from "./components/Header.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
+
+const toast = useToast();
 
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -19.99 },
@@ -36,14 +39,28 @@ const expenses = computed(() => {
     }, 0).toFixed(2);
 });
 
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  });
+
+  toast.success('Transaction added')
+}
+
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
+
 </script>
 
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
+    <Balance :total="+total" />
     <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
